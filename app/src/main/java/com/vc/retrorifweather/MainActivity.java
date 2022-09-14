7b2c9daba69c6c133d576a672d01f2387b62ec63
sarinacm@gmail.com <sarinacm@gmail.com>
2022-09-14 11:12:13 +0200

    895-login

--- app/src/main/java/com/vc/retrorifweather/MainActivity.java
+++ app/src/main/java/com/vc/retrorifweather/MainActivity.java
@@ -1,14 +1,96 @@
 package com.vc.retrorifweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText  et_email, et_password;
+    Button  btn_signup, btn_login;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_email = findViewById(R.id.et_login);
+        et_password = findViewById(R.id.et_password);
+        btn_signup = findViewById(R.id.btn_singup);
+        btn_login = findViewById(R.id.btn_login);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful())
+                                {
+                                    Toast.makeText(MainActivity.this, "Logged in ", Toast.LENGTH_SHORT).show();
+                                }
+
+                                else
+                                {
+                                    Toast.makeText(MainActivity.this, "That did not work ", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+
+
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, "Logged in ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
     }
 }
